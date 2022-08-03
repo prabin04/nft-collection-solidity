3700e1d342815cb30dfc5a1382387d67414f5a61
prabin04 <prabin.b9@gmail.com>
2022-08-03 13:24:32 +0200

    nft-app

---
+++ my-app/constants/index.js
@@ -0,0 +1,22 @@
+export const abi = [
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "whitelistedAddresses",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ];
+export const NFT_CONTRACT_ADDRESS = "0x098bE9a3Bccad03F9e62e651F6fB40a49ABdfC04";
