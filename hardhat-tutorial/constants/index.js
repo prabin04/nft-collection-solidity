68c4f879b9eed758dda007b29bc2f1a1840be3cd
prabin04 <prabin.b9@gmail.com>
2022-08-03 13:47:01 +0200

    opensea mint

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x13Ea138Ba83235293E6db644c6b0b4b8875BD1aB";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-solidity.vercel.app/api";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
